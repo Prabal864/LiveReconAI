12ff33ebd190818b01eee4e3c60ca265027f310e
prabal864 <940pps@gmail.com>
2025-08-17 13:03:25 +0530

    Removes unused imports from DynamoDBConfig to improve code clarity

--- accountservice/src/main/java/com/micronauticals/accountservice/config/DynamoDBConfig.java
+++ accountservice/src/main/java/com/micronauticals/accountservice/config/DynamoDBConfig.java
@@ -1,20 +1,15 @@
 package com.micronauticals.accountservice.config;
 
-import com.micronauticals.accountservice.entity.financialdata.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 
 @Configuration
 public class DynamoDBConfig {
