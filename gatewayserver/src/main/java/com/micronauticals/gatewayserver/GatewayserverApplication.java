831ea3b0f4a06177c88291b1b0a2a917522f4642
prabal864 <940pps@gmail.com>
2025-08-28 00:25:09 +0530

    Add request rate limiting to account and auth routes in gateway server

--- gatewayserver/src/main/java/com/micronauticals/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/micronauticals/gatewayserver/GatewayserverApplication.java
@@ -2,10 +2,13 @@ package com.micronauticals.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -29,7 +32,8 @@ public class GatewayserverApplication {
                                 .setMethods(HttpMethod.GET)
                                 .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)
                         )
-                        .circuitBreaker(config -> config.setName("AccountCircuitBreaker")))
+                        .circuitBreaker(config -> config.setName("AccountCircuitBreaker"))
+                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver())))
                         .uri("lb://ACCOUNT")
                 )
                 .route(p -> p.path("/livereconai/prod/v1/auth/**")
@@ -41,9 +45,21 @@ public class GatewayserverApplication {
                                 .setMethods(HttpMethod.GET)
                                 .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)
                         )
-                        .circuitBreaker(config -> config.setName("AuthCircuitBreaker")))
+                        .circuitBreaker(config -> config.setName("AuthCircuitBreaker"))
+                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver())))
                         .uri("lb://AUTH")
                 )
                 .build();
     }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(1,1,1);
+    }
+
+    @Bean
+    KeyResolver keyResolver(){
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+    }
+
 }
