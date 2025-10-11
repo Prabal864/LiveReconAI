5d78c85063e238c149e9c519e2775dd373febd9e
prabal864 <940pps@gmail.com>
2025-10-11 22:02:58 +0530

    Fix role case sensitivity in security configuration for /api/setu/auth/**

--- gatewayserver/src/main/java/com/micronauticals/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/micronauticals/gatewayserver/config/SecurityConfig.java
@@ -3,11 +3,8 @@ package com.micronauticals.gatewayserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -22,7 +19,7 @@ public class SecurityConfig {
         serverHttpSecurity.authorizeExchange(authorizeExchangeSpec ->
             authorizeExchangeSpec
             .pathMatchers(HttpMethod.GET).permitAll()
-            .pathMatchers("api/setu/auth/**").hasRole("User")
+            .pathMatchers("api/setu/auth/**").hasRole("USER")
         ).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
                 .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
     return serverHttpSecurity.build();
