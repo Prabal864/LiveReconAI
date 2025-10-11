1da2bd5ab0bd971925c33e510f62341c267b1855
prabal864 <940pps@gmail.com>
2025-10-11 13:29:26 +0530

    Implement KeycloakRoleConverter for JWT role extraction and update security configuration to use custom converter

--- gatewayserver/src/main/java/com/micronauticals/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/micronauticals/gatewayserver/config/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.micronauticals.gatewayserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -19,8 +23,17 @@ public class SecurityConfig {
             authorizeExchangeSpec
             .pathMatchers(HttpMethod.GET).permitAll()
             .pathMatchers("api/setu/auth/**").hasRole("User")
-        ).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+        ).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
     return serverHttpSecurity.build();
+    }
+
+    private ReactiveJwtAuthenticationConverterAdapter grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
 }
