6ad6fb947d3a418811b8526ce1557bbbcdce2ec3
prabal864 <940pps@gmail.com>
2026-01-07 13:38:56 +0530

    Change `userId` type from `Long` to `UUID` in `AuthResponse`

--- authservices/src/main/java/com/micronauticals/authservices/dto/AuthResponse.java
+++ authservices/src/main/java/com/micronauticals/authservices/dto/AuthResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -13,6 +15,6 @@ import lombok.NoArgsConstructor;
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
-    private Long userId;
+    private UUID userId;
     private String username;
 }
